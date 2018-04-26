e06444f8ae190574d21d155f5ecd9c83d5a38586
m305-59\w401 <liou880620@gmail.com>
2018-04-26 17:04:08 +0800

    rand

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 42; i++)
+            {
+                numbers.Add(i);
+            }
+
+
+            Random rand = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                int r = rand.Next(1, numbers.Count);
+                Console.WriteLine(numbers[r]);
+
+
+                numbers.RemoveAt(r);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
